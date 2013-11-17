12f06bb550712377c858ac9ac249c8cd8749d188
danielburrell <daniel.burrell@gmail.com>
2013-11-17 03:34:24 +0000

    Adjusting schema to allow CRUD on raw tables
    Granting access to pwanted to select update delete insert into tables

--- schema/base/create_database.sql
+++ schema/base/create_database.sql
@@ -351,6 +351,7 @@ BEGIN
 END$$
 
 DELIMITER ;
+SET SQL_MODE = '';
 GRANT USAGE ON *.* TO pwanted@localhost;
 GRANT EXECUTE ON procedure `wanted`.`addDetail` TO 'pwanted'@'localhost';
 GRANT EXECUTE ON procedure `wanted`.`addSchemaItem` TO 'pwanted'@'localhost';
@@ -362,6 +363,11 @@ GRANT EXECUTE ON procedure `wanted`.`getSchema` TO 'pwanted'@'localhost';
 GRANT EXECUTE ON procedure `wanted`.`getWantedList` TO 'pwanted'@'localhost';
 GRANT EXECUTE ON procedure `wanted`.`getWelcomeStatus` TO 'pwanted'@'localhost';
 GRANT EXECUTE ON procedure `wanted`.`updateUserItems` TO 'pwanted'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE `wanted`.`detail` TO 'pwanted'@'localhost';
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE `wanted`.`item` TO 'pwanted'@'localhost';
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE `wanted`.`item_schema` TO 'pwanted'@'localhost';
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE `wanted`.`schema_history` TO 'pwanted'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE `wanted`.`steam_user` TO 'pwanted'@'localhost';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
